b459f8b6a8ab3531591481dca9c62c57b7f86724
eric thul <thul.eric@gmail.com>
2014-12-01 22:55:46 -0500

    Implementing adding todos

--- examples/TodomvcF/Main.purs
+++ examples/TodomvcF/Main.purs
@@ -1,5 +1,33 @@
 module TodomvcF.Main where
 
-import Angular.Module (ngmodule')
+import Control.Monad.Eff
+import Data.Either (either)
 
-main = ngmodule' "todomvcf" []
+import Angular.Module (controller, ngmodule')
+import Angular.This (extendThis)
+
+import TodomvcF.ThisF (ForeignThis())
+import TodomvcF.TodomvcF (add)
+import TodomvcF.TodomvcI (AppT(), run)
+
+mainctrl this = extendThis { newTodo: ""
+                           , todos: []
+                           , add: add
+                           , run: (run <<< toForeignThis) this } this
+
+main = do
+  m <- ngmodule' "todomvcf" []
+  controller "Main" mainctrl' m
+
+foreign import toForeignThis """
+  function toForeignThis(that){
+    return that;
+  }
+""" :: forall a. a -> ForeignThis AppT
+
+foreign import mainctrl' """
+  /*@ngInject*/function mainctrl$prime() {
+    var impl = mainctrl(this);
+    return impl.apply(this, []);
+  }
+""" :: forall e a. Eff e Unit
--- examples/TodomvcF/TodomvcI.purs
+++ examples/TodomvcF/TodomvcI.purs
@@ -1,20 +1,31 @@
-module TodomvcF.TodomvcI where
+module TodomvcF.TodomvcI
+  ( AppT()
+  , AppE()
+  , run
+  ) where
 
+import Control.Alt ((<|>))
 import Control.Monad.Eff
-import Control.Monad.Free (FreeC(), injC)
+import Control.Monad.Free (FreeC(), injC, goEffC, goMC)
 import Data.Coyoneda (Natural())
 import Data.Either (either)
 import Data.Foreign (F(), toForeign, readString)
 import Data.Foreign.Class (readJSON)
 import Data.Functor.Coproduct (Coproduct())
+import Data.Inject (prj)
 import Data.Maybe (maybe)
+import Data.Maybe.Unsafe (fromJust)
 import qualified Data.String as S
 
-import TodomvcF.TodomvcF (TodomvcF(..), Todo(..))
+import TodomvcF.TodomvcF (Todomvc(), TodomvcF(..), Todo(..))
 import TodomvcF.StorageF (StorageF(), get, set)
+import TodomvcF.StorageI (Store(), storageN)
 import TodomvcF.ThisF (ThisF(), ForeignThis(), read, extend)
+import TodomvcF.ThisI (FThis(), thisN)
 
 type AppF = Coproduct StorageF (ThisF AppT)
+
+type AppE e = Eff (store :: Store, this :: FThis | e)
 
 type App = FreeC AppF
 
@@ -31,10 +42,24 @@ todomvcN this (Add (Todo t) a) =
       parse = \r -> either (const []) id (readString r >>= readJSON)
    in do
      todos <- injC $ maybe [] parse <$> get key
-     let todos' = [Todo { title: title, completed: false }] <> todos
+     let todos' = [Todo t { title = title }] <> todos
      injC $ set key (toForeign todos')
      AppT this' <- injC $ read this
      injC $ extend (AppT { remainingCount: this'.remainingCount + 1
                          , todos: todos'
                          , newTodo: "" }) this
      return a
+
+appN :: forall e. Natural AppF (AppE e)
+appN fa = fromJust $ (storageN <$> prj fa) <|>
+                     ((thisN :: forall e. Natural (ThisF AppT)
+                                                  (Eff (this :: FThis | e))) <$> prj fa)
+
+runTodomvc :: forall a. ForeignThis AppT -> Todomvc a -> App a
+runTodomvc t = goMC (todomvcN t)
+
+runApp :: forall e a. App a -> AppE e a
+runApp = goEffC appN
+
+run :: forall e a. ForeignThis AppT -> Todomvc a -> AppE e a
+run t = runApp <<< (runTodomvc t)
